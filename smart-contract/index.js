e9ad6c21652b504bcad252d22981ae4b66c07bf6
M Nindra Zaka <mnindrazaka@gmail.com>
2025-08-14 21:53:34 +0700

    feat: implement deployment script for contract

---
+++ smart-contract/index.js
@@ -0,0 +1,43 @@
+const { ethers } = require("ethers");
+const solc = require("solc");
+const fs = require("fs");
+const dotenv = require("dotenv");
+
+dotenv.config();
+
+const source = fs.readFileSync("index.sol", "utf8");
+
+const input = {
+  language: "Solidity",
+  sources: {
+    "index.sol": { content: source },
+  },
+  settings: {
+    outputSelection: {
+      "*": {
+        "*": ["abi", "evm.bytecode"],
+      },
+    },
+  },
+};
+
+const output = JSON.parse(solc.compile(JSON.stringify(input)));
+const contractFile = output.contracts["index.sol"]["Counter"];
+
+const abi = contractFile.abi;
+const bytecode = contractFile.evm.bytecode.object;
+
+const provider = new ethers.JsonRpcProvider(process.env.RPC_URL);
+const wallet = new ethers.Wallet(process.env.PRIVATE_KEY, provider);
+
+(async () => {
+  console.log("Deploying contract...");
+  const factory = new ethers.ContractFactory(abi, bytecode, wallet);
+  const contract = await factory.deploy();
+
+  console.log("Wait for Deployment...");
+
+  await contract.waitForDeployment();
+
+  console.log("Deployed at:", await contract.getAddress());
+})();
